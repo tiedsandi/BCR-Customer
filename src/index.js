8e0ca51ac0eee6cb44e699866819a8a0d3ddd92d
tiedSandi <wkwkwkand@gmail.com>
2022-04-17 09:46:28 +0700

    add router

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,7 +7,12 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<div>404</div>} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
